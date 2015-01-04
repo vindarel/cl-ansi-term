8be42a410e0270385fca1bc1e0965b506ad55d80
mrkkrp <markkarpov@opmbx.org>
2015-01-05 00:33:39 +0600

    initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# cl-ansi-term
+
+*Please note that this work is in progress and it's not usable yet.*
+
+This library allows to format text so that it will appear on ANSI-compliant
+terminals colorized and/or stylized.
+
+cl-ansi-term uses level of abstraction for this job: you can create
+'style-sheet' that contains as many entries as you want. Then you can format
+text with handy API.
+
+Style sheets allow specify not only colors, effects, and styles, but also
+fill-columns and other things for nice terminal output.
+
+## Installation
+
+Download or clone the repository and put it into some place where ASDF can
+find it.
+
+## Description
+
+...
+
+## Copyright
+
+Copyright (c) 2015 Mark Karpov
+
+Distributed under GNU GPL.
---
+++ cl-ansi-term.lisp
@@ -0,0 +1,29 @@
+;;; -*- Mode: Lisp; Syntax: ANSI-Common-Lisp -*-
+;;;
+;;; cl-ansi-term - library to output formatted text on ANSI-compliant
+;;; terminals.
+;;;
+;;; Copyright (c) 2015 Mark Karpov
+;;;
+;;; This program is free software: you can redistribute it and/or modify it
+;;; under the terms of the GNU General Public License as published by the
+;;; Free Software Foundation, either version 3 of the License, or (at your
+;;; option) any later version.
+;;;
+;;; This program is distributed in the hope that it will be useful, but
+;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+;;; Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License along
+;;; with this program. If not, see <http://www.gnu.org/licenses/>.
+
+(defpackage   :cl-ansi-term
+  (:nicknames :term)
+  (:use       #:common-lisp
+              #:alexandria)
+  (:export    ))
+
+(in-package #:cl-ansi-term)
+
+;;; ...
